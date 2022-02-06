e8e432794dba912a5a0205731ad3856e83456d4c
Giovanni Mazzitelli <giovanni.mazzitelli@lnf.infn.it>
2022-02-06 20:17:46 +0100

    Cygno Condor Container v0.0.1

--- conf/gettoken.sh
+++ conf/gettoken.sh
@@ -2,5 +2,8 @@
 #
 
 unset OIDC_SOCK; unset OIDCD_PID; eval `oidc-keychain`
+#
+# oidc-gen --flow device --dae $IAM_SERVER/devicecode --issuer $IAM_SERVER --scope=$IAM_SCOPES --pw-cmd="" infncloud-wlcg
+#
 oidc-gen --flow device --dae https://iam.cloud.infn.it/devicecode --issuer https://iam.cloud.infn.it --scope='openid profile email offline_access wlcg wlcg.groups' --pw-cmd="" infncloud-wlcg
 oidc-token infncloud-wlcg -s "openid profile offline_access wlcg wlcg.groups" > /tmp/token
