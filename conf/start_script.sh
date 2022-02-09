a7010a4feee432e0575fd122de0812e81b5749c2
Giovanni Mazzitelli <giovanni.mazzitelli@lnf.infn.it>
2022-02-09 16:43:59 +0100

    Cygno Condor Container v0.0.2 - temporary pach fron crontab

--- conf/start_script.sh
+++ conf/start_script.sh
@@ -4,4 +4,5 @@ echo "ciao" > /tmp/test
 #
 # install crontab
 #
-#crontab -l | { cat; echo "*/10 * * * * eval `oidc-keychain` && echo `oidc-token infncloud-wlcg -s "openid profile offline_access wlcg wlcg.groups"` > /tmp/token 2>&1"; } | crontab -
+yum install -y crontabs
+crontab -l | { cat; echo "*/10 * * * * eval \`oidc-keychain\` && echo \`oidc-token infncloud-wlcg -s \"openid profile offline_access wlcg wlcg.groups\"\` > /tmp/token"; } | crontab -
