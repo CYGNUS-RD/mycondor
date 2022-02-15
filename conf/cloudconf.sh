63d20a07cf5075b21d409e1cd21d0b8d4c172d96
Giovanni Mazzitelli <45018579+gmazzitelli@users.noreply.github.com>
2022-02-15 19:08:03 +0100

    Update cloudconf.sh

--- conf/cloudconf.sh
+++ conf/cloudconf.sh
@@ -9,6 +9,7 @@ SEC_DEFAULT_AUTHENTICATION_METHODS = SCITOKENS
 COLLECTOR_HOST = 131.154.96.173.myip.cloud.infn.it:30618
 SCHEDD_HOST = 131.154.96.173.myip.cloud.infn.it
 EOF
+### 
 cat > /usr/bin/gettoken << EOF
 echo "*/10 * * * * eval \`oidc-keychain\` && echo \`oidc-token infncloud-wlcg -s \"openid profile offline_access wlcg wlcg.groups\"\` > /tmp/token" | crontab - 
 unset OIDC_SOCK; unset OIDCD_PID; eval `oidc-keychain`
@@ -18,3 +19,5 @@ unset OIDC_SOCK; unset OIDCD_PID; eval `oidc-keychain`
 oidc-gen --flow device --dae https://iam.cloud.infn.it/devicecode --issuer https://iam.cloud.infn.it --scope='openid profile email offline_access wlcg wlcg.groups' --pw-cmd="" infncloud-wlcg
 oidc-token infncloud-wlcg -s "openid profile offline_access wlcg wlcg.groups" > /tmp/token
 EOF
+### 
+chmod +x /usr/bin/gettoken
