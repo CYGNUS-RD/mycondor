e075336c3574c5e78f3b52689eec04467e3b8edf
Giovanni Mazzitelli <giovanni.mazzitelli@lnf.infn.it>
2023-03-08 13:19:12 +0100

    v0.2

--- conf/gettoken.sh
+++ conf/gettoken.sh
@@ -5,5 +5,9 @@ unset OIDC_SOCK; unset OIDCD_PID; eval `oidc-keychain`
 #
 # oidc-gen --flow device --dae $IAM_SERVER/devicecode --issuer $IAM_SERVER --scope=$IAM_SCOPES --pw-cmd="" infncloud-wlcg
 #
-oidc-gen --flow device --dae https://iam.cloud.infn.it/devicecode --issuer https://iam.cloud.infn.it --scope='openid profile email offline_access wlcg wlcg.groups' --pw-cmd="" infncloud-wlcg
-oidc-token infncloud-wlcg -s "openid profile offline_access wlcg wlcg.groups" > /tmp/token
+oidc-gen --flow device --dae $IAM_SERVER/devicecode --issuer $IAM_SERVER --scope="$IAM_SCOPES" --pw-cmd="" $OIDC_AGENT
+oidc-token $OIDC_AGENT -s "$IAM_SCOPES" > /tmp/token
+#
+# oidc-gen --flow device --dae https://iam.cloud.infn.it/devicecode --issuer https://iam.cloud.infn.it --scope='openid profile email offline_access wlcg wlcg.groups' --pw-cmd="" infncloud-wlcg
+# oidc-token infncloud-wlcg -s "openid profile offline_access wlcg wlcg.groups" > /tmp/token
+#
--- conf/start_script.sh
+++ conf/start_script.sh
@@ -5,4 +5,5 @@ echo "ciao" > /tmp/test
 # install crontab
 #
 yum install -y crontabs
-crontab -l | { cat; echo "*/10 * * * * eval \`oidc-keychain\` && echo \`oidc-token infncloud-wlcg -s \"openid profile offline_access wlcg wlcg.groups\"\` > /tmp/token"; } | crontab -
+# crontab -l | { cat; echo "*/10 * * * * eval \`oidc-keychain\` && echo \`oidc-token $OIDC_AGENT -s \"openid profile offline_access wlcg wlcg.groups\"\` > /tmp/token"; } | crontab -
+crontab -l | { cat; echo "*/10 * * * * eval \`oidc-keychain\` && echo \`oidc-token $OIDC_AGENT \` > /tmp/token"; } | crontab -
